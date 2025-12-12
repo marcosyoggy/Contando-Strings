352d6c094c882d6702da7192973f7e302907db4a
san-gojo <marcospci2017@gmail.com>
2025-12-12 10:07:46 -0300

    contando-strings-final

--- count-str.js
+++ count-str.js
@@ -1,1 +1,26 @@
-console.log(`-------------- NEW FOLDER1111 ---------------`)
+
+const strLorem20 = 'Lorem ipsum dolor sit, amet consectetur adipisicing elit. Reiciendis adipisci enim quae corrupti optio omnis inventore vel alias possimus pariatur?'
+
+
+const countingVogals = (vogals) => {
+    
+    const countChars = vogals.reduce((acc, vogal) => {
+
+        [...strLorem20].forEach((loremChar) => {
+
+             acc[vogal] = acc[vogal] || 0
+            loremChar === vogal ? acc[vogal] += 1 : ''
+        })
+
+        return acc
+    }, {})
+
+    return countChars
+}    
+
+const alfabeth = ['A', 'E', 'I', 'O', 'U', 'B', 'C', 'D', 'F', 'G', 'H', 'J', 'K', 'L', 'M', 'N', 'P', 'Q', 'R', 'S', 'T', 'V', 'W', 'X', 'Y', 'Z']
+
+const alphabFormated = [...alfabeth.join('').toLowerCase()]
+
+log(countingVogals(alphabFormated))
+log(alphabFormated)
